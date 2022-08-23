822366381ee38c6fe1b5081adb5be6a127d51332
kevink2023 <kevink2029@gmail.com>
2022-08-23 15:09:44 -0400

    created workflow and added icon

---
+++ Workflow/Workflow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workflow
+//
+//  Created by Kevin Kelly on 8/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Kevin Kelly on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
