1dea212b0e2abcdbbf54c0c24dd26fc6dc8c64a8
kevink2023 <kevink2029@gmail.com>
2022-08-26 16:17:21 -0400

    created Lion Spell

---
+++ Lion Spell/Lion Spell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lion Spell
+//
+//  Created by Kevin Kelly on 8/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lion Spell/Lion Spell/Lion_SpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lion_SpellApp.swift
+//  Lion Spell
+//
+//  Created by Kevin Kelly on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Lion_SpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
