25f525a9a0ee3b1820877e569e753493d7cced10
kevink2023 <kevink2029@gmail.com>
2022-08-30 17:13:37 -0400

    New Files and organization

---
+++ Lion Spell/Lion Spell/LionSpellModel.swift
@@ -0,0 +1,8 @@
+//
+//  LionSpellModel.swift
+//  Lion Spell
+//
+//  Created by Kevin Kelly on 8/30/22.
+//
+
+import Foundation
---
+++ Lion Spell/Lion Spell/LionSpellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LionSpellViewModel.swift
+//  Lion Spell
+//
+//  Created by Kevin Kelly on 8/30/22.
+//
+
+import Foundation
