ff6da849e8d6561d7a6338e5faeba621fc1967d3
kevink2023 <krk252@psu.edu>
2022-09-19 15:42:14 -0400

    initial commit for Pentominoes 1

---
+++ Pentominoes/Pentominoes/PentominoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentominoesApp.swift
+//  Pentominoes
+//
+//  Created by Kevin Kelly on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PentominoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
