d343e343541046535e85e8b16342d8aebd515d0d
kevink2023 <krk252@psu.edu>
2022-09-29 12:32:04 -0400

    Created Campus

---
+++ Campus/Campus/CampusApp.swift
@@ -0,0 +1,20 @@
+//
+//  CampusApp.swift
+//  Campus
+//
+//  Created by Kevin Kelly on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CampusApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            ContentView()
+        }
+    }
+}
