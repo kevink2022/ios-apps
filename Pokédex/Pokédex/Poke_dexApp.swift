defbf05e9c5497c712c861c6d881948698f9330d
kevink2023 <krk252@psu.edu>
2022-10-19 16:20:27 -0400

    Pokédex initial commit

---
+++ Pokédex/Pokédex/Poke_dexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_dexApp.swift
+//  Pokédex
+//
+//  Created by Kevin Kelly on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Poke_dexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
