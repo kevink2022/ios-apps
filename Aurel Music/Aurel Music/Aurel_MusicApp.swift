f10e023e7c8bf28bdac79970215365d4774b591f
kevink2023 <krk252@psu.edu>
2022-10-21 14:56:50 -0400

    final project initial commit

---
+++ Aurel Music/Aurel Music/Aurel_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aurel_MusicApp.swift
+//  Aurel Music
+//
+//  Created by Kevin Kelly on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Aurel_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
